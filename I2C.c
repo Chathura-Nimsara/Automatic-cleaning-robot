6799ece17233c26b22d64e99b2fbdaeda0c5c38f
Chathura-Nimsara <95241453+Chathura-Nimsara@users.noreply.github.com>
2021-11-30 22:09:12 +0530

    Add files via upload

---
+++ I2C.c
@@ -0,0 +1,75 @@
+#include <stdint.h>
+#include <avr/io.h>
+
+#ifndef F_CPU
+#define F_CPU 8000000UL
+#endif
+
+#ifndef SCL_CLK
+#define SCL_CLK 10000L
+#endif
+
+/* Define bit rate */
+#define BITRATE(TWSR)((F_CPU/SCL_CLK)-16)/(2*pow(4,(TWSR&((1<<TWPS0)|(1<<TWPS1)))))
+
+
+void I2C_Init()			/* I2C initialize function */
+{
+	TWSR=0x00;
+	TWBR = BITRATE(TWSR);	/* Get bit rate register value by formula */
+}
+
+unsigned char I2C_Start(char write_address)/* I2C start function */
+{
+	unsigned char status;		/* Declare variable */
+	TWCR=(1<<TWSTA)|(1<<TWEN)|(1<<TWINT); /* Enable TWI, generate START */
+	while(!(TWCR&(1<<TWINT)));	/* Wait until TWI finish its current job */
+	status=TWSR&0xF8;		/* Read TWI status register */
+	if(status!=0x08)		/* Check weather START transmitted or not? */
+	return 0;			/* Return 0 to indicate start condition fail */
+	TWDR= write_address;		/* Write SLA+W in TWI data register */
+	TWCR=(1<<TWEN)|(1<<TWINT);	/* Enable TWI & clear interrupt flag */
+	while(!(TWCR&(1<<TWINT)));	/* Wait until TWI finish its current job */
+	status=TWSR&0xF8;		/* Read TWI status register */
+	if(status==0x18)		/* Check for SLA+W transmitted &ack received */
+	return 1;			/* Return 1 to indicate ack received */
+	if(status==0x20)		/* Check for SLA+W transmitted &nack received */
+	return 2;			/* Return 2 to indicate nack received */
+	else
+	return 3;			/* Else return 3 to indicate SLA+W failed */
+}
+
+uint8_t I2C_Write(char chardata)	/* I2C write function */
+{
+	uint8_t status;		/* Declare variable */
+	TWDR= chardata;			/* Copy data in TWI data register */
+	TWCR=(1<<TWEN)|(1<<TWINT);	/* Enable TWI and clear interrupt flag */
+	while(!(TWCR&(1<<TWINT)));	/* Wait until TWI finish its current job */
+	status=TWSR&0xF8;		/* Read TWI status register */
+	if(status==0x28)		/* Check for data transmitted &ack received */
+	return 0;			/* Return 0 to indicate ack received */
+	if(status==0x30)		/* Check for data transmitted &nack received */
+	return 1;			/* Return 1 to indicate nack received */
+	else
+	return 2;			/* Else return 2 for data transmission failure */
+}
+
+char I2C_Read_Ack()		/* I2C read ack function */
+{
+	TWCR=(1<<TWEN)|(1<<TWINT)|(1<<TWEA); /* Enable TWI, generation of ack */
+	while(!(TWCR&(1<<TWINT)));	/* Wait until TWI finish its current job */
+	return TWDR;			/* Return received data */
+}
+
+char I2C_Read_Nack()		/* I2C read nack function */
+{
+	TWCR=(1<<TWEN)|(1<<TWINT);	/* Enable TWI and clear interrupt flag */
+	while(!(TWCR&(1<<TWINT)));	/* Wait until TWI finish its current job */
+	return TWDR;		/* Return received data */
+
+}
+void I2C_Stop()			/* I2C stop function */
+{
+	TWCR=(1<<TWSTO)|(1<<TWINT)|(1<<TWEN);/* Enable TWI, generate stop */
+	while(TWCR&(1<<TWSTO));	/* Wait until stop condition execution */
+}
